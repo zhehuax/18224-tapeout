d03526e1dba7d108395a54b3b7dd414540cd1894
Zhehua Xiang <zhehuax@ece010.ece.local.cmu.edu>
2024-02-15 15:06:20 -0500

    added rngefinder

--- src/chip.sv
+++ src/chip.sv
@@ -6,10 +6,83 @@ module my_chip (
     input logic clock,
     input logic reset // Important: Reset is ACTIVE-HIGH
 );
+
+    // RangeFinder stuff here
+    logic [7:0] data_in;
+    logic go, finish;
+    logic [7:0] range;
+    logic debug_error;
+
+    logic update_max, update_min;
+    logic [7:0] max, min;
+    enum logic {REST=1'd0, ON=1'd1} state, nextState;
+
+    assign data_in = io_in[7:0];
+    assign range = io_out[7:0];
+    assign go = io_in[8];
+    assign finish = io_in[9];
+    assign debug_error = io_out[8];
+
+    always_comb begin
+        unique case(state)
+            REST:   begin
+                        update_max = go;
+                        update_min = go;
+                        nextState = (go && ~finish)? ON : REST;
+                    end
+
+            ON:     begin
+                        if (finish) begin
+                            update_max = 0;
+                            update_min = 0;
+                            nextState = REST;
+                        end
+
+                        else begin
+                            update_max = (data_in>max);
+                            update_min = (data_in<min);
+                            nextState = ON;
+                        end
+                    end 
+
+
+            default: nextState = REST;
+        endcase
+    end
+
+    always_ff @(posedge clock, posedge reset) begin
+        if (reset) begin
+            state <= REST;
+            max <= 'd0;
+            min <= 'd0;
+            debug_error <= 0;
+            range <= 'd0;
+        end
+
+        else begin
+            state <= nextState;
+            if (finish) begin
+                if (data_in>max) range <= data_in - min;
+                else if (data_in<min) range <= max-data_in;
+                else range <= max-min;
+                max <='d0;
+                min <= 'd0;
+            end
+
+            else begin
+                if (update_max) max <= data_in;
+                if (update_min) min <= data_in;
+            end
+
+            if (state == REST && finish) debug_error <= 1;
+            if (go && ~finish) debug_error <= 0;
+                
+        end
+    end
     
     // Basic counter design as an example
 
-
+    /*
     wire [6:0] led_out;
     assign io_out[6:0] = led_out;
 
@@ -43,5 +116,6 @@ module my_chip (
 
     // instantiate segment display
     seg7 seg7(.counter(digit), .segments(led_out));
+    */
 
 endmodule
