65e9b19cd8720651aa8cf032990dc94c20339759
Zhehua Xiang <zhehuax@ece010.ece.local.cmu.edu>
2024-04-25 00:42:49 -0400

    fix2

--- src/csrc/filelist.cu
+++ src/csrc/filelist.cu
@@ -1,12 +1,20 @@
 PIC_LD=ld
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _29537_archive_1.so
-_29537_archive_1.so : archive.0/_29537_archive_1.a
+ARCHIVE_OBJS += _870_archive_1.so
+_870_archive_1.so : archive.1/_870_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -o .//../simv.daidir//_29537_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -o .//../simv.daidir//_870_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../simv.daidir//_29537_archive_1.so $@
+	@ln -sf .//../simv.daidir//_870_archive_1.so $@
+
+
+ARCHIVE_OBJS += _prev_archive_1.so
+_prev_archive_1.so : archive.1/_prev_archive_1.a
+	@$(AR) -s $<
+	@$(PIC_LD) -shared  -o .//../simv.daidir//_prev_archive_1.so --whole-archive $< --no-whole-archive
+	@rm -f $@
+	@ln -sf .//../simv.daidir//_prev_archive_1.so $@
 
 
 
