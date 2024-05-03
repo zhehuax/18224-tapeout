cde25160383c8fb631ebfa2cce1cf737f686e213
Zhehua Xiang <zhehuax@ece010.ece.local.cmu.edu>
2024-05-03 16:17:23 -0400

    area test for div part1

--- src/csrc/filelist.cu
+++ src/csrc/filelist.cu
@@ -1,16 +1,16 @@
 PIC_LD=ld
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _870_archive_1.so
-_870_archive_1.so : archive.1/_870_archive_1.a
+ARCHIVE_OBJS += _32640_archive_1.so
+_32640_archive_1.so : archive.2/_32640_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -o .//../simv.daidir//_870_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -o .//../simv.daidir//_32640_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../simv.daidir//_870_archive_1.so $@
+	@ln -sf .//../simv.daidir//_32640_archive_1.so $@
 
 
 ARCHIVE_OBJS += _prev_archive_1.so
-_prev_archive_1.so : archive.1/_prev_archive_1.a
+_prev_archive_1.so : archive.2/_prev_archive_1.a
 	@$(AR) -s $<
 	@$(PIC_LD) -shared  -o .//../simv.daidir//_prev_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
